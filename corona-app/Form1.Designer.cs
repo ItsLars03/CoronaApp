cc5162408947fe0295ade6e7db864a117764777d
supr3mion <230690@edu.rocfriesepoort.nl>
2021-01-18 15:24:19 +0100

    applicatie af

--- corona-app/Form1.Designer.cs
+++ corona-app/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace corona_app
 {
-    partial class Form1
+    partial class MainScreen
     {
         /// <summary>
         /// Required designer variable.
@@ -29,22 +29,369 @@ namespace corona_app
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainScreen));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnlVraag1 = new System.Windows.Forms.Panel();
+            this.errorPanelVraag1 = new System.Windows.Forms.Panel();
+            this.errorLabel1 = new System.Windows.Forms.Label();
+            this.pnlVraag2 = new System.Windows.Forms.Panel();
+            this.errorPanelVraag2 = new System.Windows.Forms.Panel();
+            this.errorLabel2 = new System.Windows.Forms.Label();
+            this.pnlVraag3 = new System.Windows.Forms.Panel();
+            this.pnlVraag4 = new System.Windows.Forms.Panel();
+            this.lblVraag = new System.Windows.Forms.Label();
+            this.tbVraag4 = new System.Windows.Forms.TextBox();
+            this.errorPanelVraag4 = new System.Windows.Forms.Panel();
+            this.errorLabel4 = new System.Windows.Forms.Label();
+            this.btnVraag4 = new System.Windows.Forms.Button();
+            this.lblVraag4 = new System.Windows.Forms.Label();
+            this.errorPanelVraag3 = new System.Windows.Forms.Panel();
+            this.errorLabel3 = new System.Windows.Forms.Label();
+            this.btnVraag3 = new System.Windows.Forms.Button();
+            this.lblVraag3 = new System.Windows.Forms.Label();
+            this.rbtnVraag3NEE = new System.Windows.Forms.RadioButton();
+            this.rbtnVraag3JA = new System.Windows.Forms.RadioButton();
+            this.btnVraag2 = new System.Windows.Forms.Button();
+            this.lblVraag2 = new System.Windows.Forms.Label();
+            this.rbtnVraag2NEE = new System.Windows.Forms.RadioButton();
+            this.rbtnVraag2JA = new System.Windows.Forms.RadioButton();
+            this.btnVraag1 = new System.Windows.Forms.Button();
+            this.lblVraag1 = new System.Windows.Forms.Label();
+            this.rbtnVraag1NEE = new System.Windows.Forms.RadioButton();
+            this.rbtnVraag1JA = new System.Windows.Forms.RadioButton();
+            this.imgGGD = new System.Windows.Forms.Label();
+            this.pnlAdvies = new System.Windows.Forms.Panel();
+            this.lblAdvies = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.pnlVraag1.SuspendLayout();
+            this.errorPanelVraag1.SuspendLayout();
+            this.pnlVraag2.SuspendLayout();
+            this.errorPanelVraag2.SuspendLayout();
+            this.pnlVraag3.SuspendLayout();
+            this.pnlVraag4.SuspendLayout();
+            this.errorPanelVraag4.SuspendLayout();
+            this.errorPanelVraag3.SuspendLayout();
+            this.pnlAdvies.SuspendLayout();
             this.SuspendLayout();
             // 
-            // Form1
+            // btnStart
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.btnStart.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.btnStart, "btnStart");
+            this.btnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStart.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pnlVraag1
+            // 
+            this.pnlVraag1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.pnlVraag1.Controls.Add(this.errorPanelVraag1);
+            this.pnlVraag1.Controls.Add(this.pnlVraag2);
+            this.pnlVraag1.Controls.Add(this.btnVraag1);
+            this.pnlVraag1.Controls.Add(this.lblVraag1);
+            this.pnlVraag1.Controls.Add(this.rbtnVraag1NEE);
+            this.pnlVraag1.Controls.Add(this.rbtnVraag1JA);
+            this.pnlVraag1.Cursor = System.Windows.Forms.Cursors.Default;
+            resources.ApplyResources(this.pnlVraag1, "pnlVraag1");
+            this.pnlVraag1.Name = "pnlVraag1";
+            this.pnlVraag1.Paint += new System.Windows.Forms.PaintEventHandler(this.pnlVraag1_Paint);
+            // 
+            // errorPanelVraag1
+            // 
+            this.errorPanelVraag1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.errorPanelVraag1.Controls.Add(this.errorLabel1);
+            resources.ApplyResources(this.errorPanelVraag1, "errorPanelVraag1");
+            this.errorPanelVraag1.Name = "errorPanelVraag1";
+            // 
+            // errorLabel1
+            // 
+            resources.ApplyResources(this.errorLabel1, "errorLabel1");
+            this.errorLabel1.Name = "errorLabel1";
+            // 
+            // pnlVraag2
+            // 
+            this.pnlVraag2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.pnlVraag2.Controls.Add(this.errorPanelVraag2);
+            this.pnlVraag2.Controls.Add(this.pnlVraag3);
+            this.pnlVraag2.Controls.Add(this.btnVraag2);
+            this.pnlVraag2.Controls.Add(this.lblVraag2);
+            this.pnlVraag2.Controls.Add(this.rbtnVraag2NEE);
+            this.pnlVraag2.Controls.Add(this.rbtnVraag2JA);
+            resources.ApplyResources(this.pnlVraag2, "pnlVraag2");
+            this.pnlVraag2.Name = "pnlVraag2";
+            // 
+            // errorPanelVraag2
+            // 
+            this.errorPanelVraag2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.errorPanelVraag2.Controls.Add(this.errorLabel2);
+            resources.ApplyResources(this.errorPanelVraag2, "errorPanelVraag2");
+            this.errorPanelVraag2.Name = "errorPanelVraag2";
+            // 
+            // errorLabel2
+            // 
+            resources.ApplyResources(this.errorLabel2, "errorLabel2");
+            this.errorLabel2.Name = "errorLabel2";
+            // 
+            // pnlVraag3
+            // 
+            this.pnlVraag3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.pnlVraag3.Controls.Add(this.pnlVraag4);
+            this.pnlVraag3.Controls.Add(this.errorPanelVraag3);
+            this.pnlVraag3.Controls.Add(this.btnVraag3);
+            this.pnlVraag3.Controls.Add(this.lblVraag3);
+            this.pnlVraag3.Controls.Add(this.rbtnVraag3NEE);
+            this.pnlVraag3.Controls.Add(this.rbtnVraag3JA);
+            resources.ApplyResources(this.pnlVraag3, "pnlVraag3");
+            this.pnlVraag3.Name = "pnlVraag3";
+            // 
+            // pnlVraag4
+            // 
+            this.pnlVraag4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.pnlVraag4.Controls.Add(this.lblVraag);
+            this.pnlVraag4.Controls.Add(this.tbVraag4);
+            this.pnlVraag4.Controls.Add(this.errorPanelVraag4);
+            this.pnlVraag4.Controls.Add(this.btnVraag4);
+            this.pnlVraag4.Controls.Add(this.lblVraag4);
+            resources.ApplyResources(this.pnlVraag4, "pnlVraag4");
+            this.pnlVraag4.Name = "pnlVraag4";
+            // 
+            // lblVraag
+            // 
+            resources.ApplyResources(this.lblVraag, "lblVraag");
+            this.lblVraag.Name = "lblVraag";
+            // 
+            // tbVraag4
+            // 
+            resources.ApplyResources(this.tbVraag4, "tbVraag4");
+            this.tbVraag4.Name = "tbVraag4";
+            // 
+            // errorPanelVraag4
+            // 
+            this.errorPanelVraag4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.errorPanelVraag4.Controls.Add(this.errorLabel4);
+            resources.ApplyResources(this.errorPanelVraag4, "errorPanelVraag4");
+            this.errorPanelVraag4.Name = "errorPanelVraag4";
+            // 
+            // errorLabel4
+            // 
+            resources.ApplyResources(this.errorLabel4, "errorLabel4");
+            this.errorLabel4.Name = "errorLabel4";
+            // 
+            // btnVraag4
+            // 
+            this.btnVraag4.BackColor = System.Drawing.Color.White;
+            this.btnVraag4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVraag4.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btnVraag4, "btnVraag4");
+            this.btnVraag4.Name = "btnVraag4";
+            this.btnVraag4.UseVisualStyleBackColor = false;
+            this.btnVraag4.Click += new System.EventHandler(this.btnVraag4_Click);
+            // 
+            // lblVraag4
+            // 
+            resources.ApplyResources(this.lblVraag4, "lblVraag4");
+            this.lblVraag4.Name = "lblVraag4";
+            // 
+            // errorPanelVraag3
+            // 
+            this.errorPanelVraag3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.errorPanelVraag3.Controls.Add(this.errorLabel3);
+            resources.ApplyResources(this.errorPanelVraag3, "errorPanelVraag3");
+            this.errorPanelVraag3.Name = "errorPanelVraag3";
+            // 
+            // errorLabel3
+            // 
+            resources.ApplyResources(this.errorLabel3, "errorLabel3");
+            this.errorLabel3.Name = "errorLabel3";
+            // 
+            // btnVraag3
+            // 
+            this.btnVraag3.BackColor = System.Drawing.Color.White;
+            this.btnVraag3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVraag3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btnVraag3, "btnVraag3");
+            this.btnVraag3.Name = "btnVraag3";
+            this.btnVraag3.UseVisualStyleBackColor = false;
+            this.btnVraag3.Click += new System.EventHandler(this.btnVraag3_Click);
+            // 
+            // lblVraag3
+            // 
+            resources.ApplyResources(this.lblVraag3, "lblVraag3");
+            this.lblVraag3.Name = "lblVraag3";
+            // 
+            // rbtnVraag3NEE
+            // 
+            resources.ApplyResources(this.rbtnVraag3NEE, "rbtnVraag3NEE");
+            this.rbtnVraag3NEE.Name = "rbtnVraag3NEE";
+            this.rbtnVraag3NEE.TabStop = true;
+            this.rbtnVraag3NEE.UseVisualStyleBackColor = true;
+            // 
+            // rbtnVraag3JA
+            // 
+            resources.ApplyResources(this.rbtnVraag3JA, "rbtnVraag3JA");
+            this.rbtnVraag3JA.Name = "rbtnVraag3JA";
+            this.rbtnVraag3JA.TabStop = true;
+            this.rbtnVraag3JA.UseVisualStyleBackColor = true;
+            // 
+            // btnVraag2
+            // 
+            this.btnVraag2.BackColor = System.Drawing.Color.White;
+            this.btnVraag2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVraag2.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btnVraag2, "btnVraag2");
+            this.btnVraag2.Name = "btnVraag2";
+            this.btnVraag2.UseVisualStyleBackColor = false;
+            this.btnVraag2.Click += new System.EventHandler(this.btnVraag2_Click);
+            // 
+            // lblVraag2
+            // 
+            resources.ApplyResources(this.lblVraag2, "lblVraag2");
+            this.lblVraag2.Name = "lblVraag2";
+            // 
+            // rbtnVraag2NEE
+            // 
+            resources.ApplyResources(this.rbtnVraag2NEE, "rbtnVraag2NEE");
+            this.rbtnVraag2NEE.Name = "rbtnVraag2NEE";
+            this.rbtnVraag2NEE.TabStop = true;
+            this.rbtnVraag2NEE.UseVisualStyleBackColor = true;
+            // 
+            // rbtnVraag2JA
+            // 
+            resources.ApplyResources(this.rbtnVraag2JA, "rbtnVraag2JA");
+            this.rbtnVraag2JA.Name = "rbtnVraag2JA";
+            this.rbtnVraag2JA.TabStop = true;
+            this.rbtnVraag2JA.UseVisualStyleBackColor = true;
+            // 
+            // btnVraag1
+            // 
+            this.btnVraag1.BackColor = System.Drawing.Color.White;
+            this.btnVraag1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnVraag1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.btnVraag1, "btnVraag1");
+            this.btnVraag1.Name = "btnVraag1";
+            this.btnVraag1.UseVisualStyleBackColor = false;
+            this.btnVraag1.Click += new System.EventHandler(this.btnVraag1_Click);
+            // 
+            // lblVraag1
+            // 
+            resources.ApplyResources(this.lblVraag1, "lblVraag1");
+            this.lblVraag1.Name = "lblVraag1";
+            // 
+            // rbtnVraag1NEE
+            // 
+            resources.ApplyResources(this.rbtnVraag1NEE, "rbtnVraag1NEE");
+            this.rbtnVraag1NEE.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbtnVraag1NEE.Name = "rbtnVraag1NEE";
+            this.rbtnVraag1NEE.TabStop = true;
+            this.rbtnVraag1NEE.UseVisualStyleBackColor = true;
+            // 
+            // rbtnVraag1JA
+            // 
+            resources.ApplyResources(this.rbtnVraag1JA, "rbtnVraag1JA");
+            this.rbtnVraag1JA.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.rbtnVraag1JA.Name = "rbtnVraag1JA";
+            this.rbtnVraag1JA.TabStop = true;
+            this.rbtnVraag1JA.UseVisualStyleBackColor = true;
+            // 
+            // imgGGD
+            // 
+            this.imgGGD.Image = global::corona_app.Properties.Resources.NoPath___kopie__3_;
+            resources.ApplyResources(this.imgGGD, "imgGGD");
+            this.imgGGD.Name = "imgGGD";
+            // 
+            // pnlAdvies
+            // 
+            this.pnlAdvies.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.pnlAdvies.Controls.Add(this.btnReset);
+            this.pnlAdvies.Controls.Add(this.lblAdvies);
+            resources.ApplyResources(this.pnlAdvies, "pnlAdvies");
+            this.pnlAdvies.Name = "pnlAdvies";
+            // 
+            // lblAdvies
+            // 
+            this.lblAdvies.BackColor = System.Drawing.Color.White;
+            resources.ApplyResources(this.lblAdvies, "lblAdvies");
+            this.lblAdvies.Name = "lblAdvies";
+            // 
+            // btnReset
+            // 
+            this.btnReset.BackColor = System.Drawing.Color.White;
+            this.btnReset.Cursor = System.Windows.Forms.Cursors.Hand;
+            resources.ApplyResources(this.btnReset, "btnReset");
+            this.btnReset.Name = "btnReset";
+            this.btnReset.UseVisualStyleBackColor = false;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // MainScreen
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(151)))), ((int)(((byte)(193)))), ((int)(((byte)(233)))));
+            this.CancelButton = this.btnStart;
+            this.Controls.Add(this.pnlAdvies);
+            this.Controls.Add(this.pnlVraag1);
+            this.Controls.Add(this.imgGGD);
+            this.Controls.Add(this.btnStart);
+            this.Name = "MainScreen";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.pnlVraag1.ResumeLayout(false);
+            this.pnlVraag1.PerformLayout();
+            this.errorPanelVraag1.ResumeLayout(false);
+            this.errorPanelVraag1.PerformLayout();
+            this.pnlVraag2.ResumeLayout(false);
+            this.pnlVraag2.PerformLayout();
+            this.errorPanelVraag2.ResumeLayout(false);
+            this.errorPanelVraag2.PerformLayout();
+            this.pnlVraag3.ResumeLayout(false);
+            this.pnlVraag3.PerformLayout();
+            this.pnlVraag4.ResumeLayout(false);
+            this.pnlVraag4.PerformLayout();
+            this.errorPanelVraag4.ResumeLayout(false);
+            this.errorPanelVraag4.PerformLayout();
+            this.errorPanelVraag3.ResumeLayout(false);
+            this.errorPanelVraag3.PerformLayout();
+            this.pnlAdvies.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label imgGGD;
+        private System.Windows.Forms.Panel pnlVraag1;
+        private System.Windows.Forms.Label lblVraag1;
+        private System.Windows.Forms.RadioButton rbtnVraag1NEE;
+        private System.Windows.Forms.RadioButton rbtnVraag1JA;
+        private System.Windows.Forms.Panel pnlVraag2;
+        private System.Windows.Forms.Button btnVraag2;
+        private System.Windows.Forms.Label lblVraag2;
+        private System.Windows.Forms.RadioButton rbtnVraag2NEE;
+        private System.Windows.Forms.RadioButton rbtnVraag2JA;
+        private System.Windows.Forms.Panel errorPanelVraag1;
+        private System.Windows.Forms.Label errorLabel1;
+        private System.Windows.Forms.Panel errorPanelVraag2;
+        private System.Windows.Forms.Label errorLabel2;
+        private System.Windows.Forms.Panel pnlVraag3;
+        private System.Windows.Forms.Panel errorPanelVraag3;
+        private System.Windows.Forms.Label errorLabel3;
+        private System.Windows.Forms.Button btnVraag3;
+        private System.Windows.Forms.Label lblVraag3;
+        private System.Windows.Forms.RadioButton rbtnVraag3NEE;
+        private System.Windows.Forms.RadioButton rbtnVraag3JA;
+        private System.Windows.Forms.Button btnVraag1;
+        private System.Windows.Forms.Panel pnlVraag4;
+        private System.Windows.Forms.Label lblVraag;
+        private System.Windows.Forms.TextBox tbVraag4;
+        private System.Windows.Forms.Panel errorPanelVraag4;
+        private System.Windows.Forms.Label errorLabel4;
+        private System.Windows.Forms.Button btnVraag4;
+        private System.Windows.Forms.Label lblVraag4;
+        private System.Windows.Forms.Panel pnlAdvies;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Label lblAdvies;
     }
 }
 
--- corona-app/Form1.cs
+++ corona-app/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace corona_app
 {
-    public partial class Form1 : Form
+    public partial class MainScreen : Form
     {
-        public Form1()
+        public MainScreen()
         {
             InitializeComponent();
         }
@@ -20,6 +20,103 @@ namespace corona_app
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+
+        private void pnlVraag1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+                pnlVraag1.Show();
+        }
+
+        private void btnVraag1_Click(object sender, EventArgs e)
+        {
+            if (rbtnVraag1JA.Checked == true || rbtnVraag1NEE.Checked == true)
+            {
+                pnlVraag2.Show();
+                errorPanelVraag1.Hide();
+
+            } else
+            {
+                errorPanelVraag1.Show();
+            }
+        }
+
+        private void btnVraag2_Click(object sender, EventArgs e)
+        {
+            if (rbtnVraag2JA.Checked == true || rbtnVraag2NEE.Checked == true)
+            {
+                pnlVraag3.Show();
+                errorPanelVraag2.Hide();
+
+            } else
+            {
+                errorPanelVraag2.Show();
+            }
+        }
+
+        private void btnVraag3_Click(object sender, EventArgs e)
+        {
+            if (rbtnVraag3JA.Checked == true || rbtnVraag3NEE.Checked == true)
+            {
+                pnlVraag4.Show();
+                errorPanelVraag3.Hide();
+            } else
+            {
+                errorPanelVraag3.Show();
+            }
+        }
+
+        private void btnVraag4_Click(object sender, EventArgs e)
+        {
+            double temperatuur = 0.0;
+
+            try 
+            { 
+                temperatuur = double.Parse(tbVraag4.Text);
+
+            } catch (Exception)
+            {
+                errorPanelVraag4.Show();
+            }
+
+            if (temperatuur >= 32 && temperatuur <= 44)
+            {
+                pnlAdvies.Show();
+                pnlVraag1.Hide();
+                pnlVraag2.Hide();
+                pnlVraag3.Hide();
+                pnlVraag4.Hide();
+                errorPanelVraag4.Hide();
+                if ((temperatuur <= 36.5 || temperatuur >= 37.5) || rbtnVraag1JA.Checked == true || rbtnVraag2JA.Checked == true || rbtnVraag3JA.Checked == true)
+                {
+                    lblAdvies.Text = "Blijf thuis, bel de GGD en laat je testen";
+                } else
+                {
+                    lblAdvies.Text = "Niks aan de hand";
+                }
+            }
+            else
+            {
+                errorPanelVraag4.Show();
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            pnlAdvies.Hide();
+            rbtnVraag1JA.Checked = false;
+            rbtnVraag1NEE.Checked = false;
+            rbtnVraag2JA.Checked = false;
+            rbtnVraag2NEE.Checked = false;
+            rbtnVraag3JA.Checked = false;
+            rbtnVraag3NEE.Checked = false;
+            tbVraag4.Text = "";
+            lblAdvies.Text = "";
         }
     }
 }
